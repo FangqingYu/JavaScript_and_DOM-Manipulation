dbb3761c884793165312bee5faf89085d3656e2a
Fangqing Yu <francis.fq.yu@gmail.com>
2019-01-28 17:40:09 -0800

    2nd filter done

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -20,15 +20,44 @@ submit.on("click", function() {
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  // date input
+  var inputElement1 = d3.select("#datetime");
+  // city input
+  var inputElement2 = d3.select("#city");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var inputValue1 = inputElement1.property("value");
+  var inputValue2 = inputElement2.property("value");
 
-  console.log(inputValue);
+  console.log(inputValue1);
+  console.log(inputValue2);
   //console.log(tableData);
 
-  var filteredData = tableData.filter( sighting => sighting.datetime === inputValue);
+  var filteredData = tableData.filter( sighting => {
+
+    // When there is a date input and 
+    if(inputValue1) {
+      // when there is also a city input, return entries that satisfy both condition
+      if(inputValue2) {
+        return sighting.datetime === inputValue1 && sighting.city === inputValue2
+      }
+      // when there is no city input, return entires that satisfy first condition
+      else {
+        return sighting.datetime === inputValue1
+      }
+    }
+    // When there is no date input and
+    else {
+      //when there is a city input, return entries that satisfy the second condition
+      if(inputValue2) {
+        return sighting.city === inputValue2
+      }
+      // When no input are received, return entire dataset
+      else {
+        return sighting
+      }
+    }
+  });
 
   console.log(filteredData);
   
@@ -49,16 +78,5 @@ submit.on("click", function() {
 
 });
 
-/* function buildTable(Data) {
- tbody.html("");
-  var tbody = d3.select("tbody");
 
-  Data.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);v
-};
-
-buildTable(filteredDatav); */
 
